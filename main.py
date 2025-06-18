2d7622dda89a118445f4bc4c027f52b00cd1edb8
Jepoy <poyhidalgo@gmail.com>
2025-06-18 20:28:59 +0800

    chore: add gitignore and initializing the project

---
+++ main.py
@@ -0,0 +1,39 @@
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+model = ChatOpenAI(
+    model="gpt-4.1",
+    temperature=0,
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+)
+
+server_params = StdioServerParameters(
+    command="npx",
+    env={
+        "FIRECRAWL_API_KEY": os.getenv("FIRECRAWL_API_KEY"),
+    },
+    args=["firecrawl-mcp"]
+)
+
+async def main():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            tools = await load_mcp_tools(session)
+            agent = create_react_agent(tools)
+
+            message = [
+                {
+                    "role": "system",
+                    "content": "You are a computer scientist assistant",
+
+                }
+            ]
